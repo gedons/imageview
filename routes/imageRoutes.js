b1fb566be8f5f585d3890c8f38be0338dafab455
Nerdy Gedoni <gedoniani@gmail.com>
2024-07-31 15:59:15 +0100

    update

--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.post('/upload', upload.single('image'), uploadImageController);
 router.get('/', getAllImagesController);
 router.get('/:id', getImageByIdController);
-router.put('/:id/edit', editImageController);
+router.put('/edit/:id', editImageController);
 router.delete('/:id', deleteImageController);
 
 module.exports = router;
